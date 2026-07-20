9cb146120d78a29a5fb7bb8f8921e59898a864e9
rjsprncssnn13 <princessannierojas1213@gmail.com>
2026-07-20 12:25:41 +0800

    Update header.js

--- header/header.js
+++ header/header.js
@@ -151,4 +151,14 @@ function setupHeaderInteractivity() {
             smoothScrollTo(targetId);
         });
     });
+
+    const toggleTheme = () => {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    };
 }
